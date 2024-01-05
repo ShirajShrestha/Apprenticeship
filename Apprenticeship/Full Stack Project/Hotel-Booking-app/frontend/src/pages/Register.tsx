7501fa869652ec79101fac1a96fe449b8fcfad26
ShirajShrestha <shresthashiraj333@gmail.com>
2024-01-05 15:30:22 +0545

    Registration page added

---
+++ Apprenticeship/Full Stack Project/Hotel-Booking-app/frontend/src/pages/Register.tsx
@@ -0,0 +1,125 @@
+import { useForm } from "react-hook-form";
+import "../styles/register.css";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
+
+export type RegisterFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const Register = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
+
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: () => {
+      console.log("Registration Successful");
+    },
+    onError: (error: Error) => {
+      console.log(error.message);
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
+  return (
+    <>
+      <form className="form-container" onSubmit={onSubmit}>
+        <h2 className="title-text">Create an Account</h2>
+        <div className="name-wrapper">
+          <label htmlFor="first-name" className="user-input first-name">
+            First Name
+            <input
+              type="text"
+              className="name-input "
+              {...register("firstName", { required: "This field is required" })}
+            />
+            {errors.firstName && (
+              <span className="text-red-500">{errors.firstName.message} </span>
+            )}
+          </label>
+          <label htmlFor="last-name" className="user-input last-name">
+            Last Name
+            <input
+              type="text"
+              className="name-input "
+              {...register("lastName", { required: "This field is required" })}
+            />
+            {errors.lastName && (
+              <span className="text-red-500">{errors.lastName.message} </span>
+            )}
+          </label>
+        </div>
+        <label htmlFor="email" className="user-input email">
+          Email
+          <input
+            type="email"
+            className="name-input "
+            {...register("email", { required: "This field is required" })}
+          />
+          {errors.email && (
+            <span className="text-red-500">{errors.email.message} </span>
+          )}
+        </label>
+        <label htmlFor="password" className="user-input password">
+          Password
+          <input
+            type="password"
+            className="name-input "
+            {...register("password", {
+              required: "This field is required",
+              minLength: {
+                value: 8,
+                message: "Password must be atleast 8 characters",
+              },
+            })}
+          />
+          {errors.password && (
+            <span className="text-red-500">{errors.password.message} </span>
+          )}
+        </label>
+        <label
+          htmlFor="confirm-password"
+          className="user-input confirm-password"
+        >
+          Confirm Password
+          <input
+            type="password"
+            className="name-input "
+            {...register("confirmPassword", {
+              validate: (val) => {
+                if (!val) {
+                  return "This field is required";
+                } else if (watch("password") !== val) {
+                  return "Your password do not match";
+                }
+              },
+            })}
+          />
+          {errors.confirmPassword && (
+            <span className="text-red-500">
+              {errors.confirmPassword.message}{" "}
+            </span>
+          )}
+        </label>
+        <span>
+          <button className="btn bg-blue-700" type="submit">
+            Create Account
+          </button>
+        </span>
+      </form>
+    </>
+  );
+};
+
+export default Register;
