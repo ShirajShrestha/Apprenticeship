efc56108da3fa29a8d7746a993baa9b0ee088646
ShirajShrestha <shresthashiraj333@gmail.com>
2024-01-12 23:40:14 +0545

    Add Search feat

--- Apprenticeship/Full Stack Project/Hotel-Booking-app/frontend/src/pages/Search.tsx
+++ Apprenticeship/Full Stack Project/Hotel-Booking-app/frontend/src/pages/Search.tsx
@@ -1,9 +1,110 @@
+import { useQuery } from "react-query";
 import { useSearchContext } from "../contexts/SearchContext";
+import * as apiClient from "../api-client";
+import { useState } from "react";
+import SearchResultsCard from "../components/SearchResultsCard";
+import Pagination from "../components/Pagination";
+import StarRatingFilter from "../components/StarRatingFilter";
+import HotelTypesFilter from "../components/HotelTypesFilter";
+import FacilitiesFilter from "../components/FacilitiesFilter";
 
 const Search = () => {
   const search = useSearchContext();
-  console.log(search);
-  return <div>search page</div>;
+  const [page, setPage] = useState<number>(1);
+  const [selectedStars, setSelectedStars] = useState<string[]>([]);
+  const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
+  const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
+
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+    page: page.toString(),
+    stars: selectedStars,
+    types: selectedHotelTypes,
+    facilities: selectedFacilities,
+  };
+
+  const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
+    apiClient.searchHotels(searchParams)
+  );
+
+  const handleStarsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const starRating = event.target.value;
+
+    setSelectedStars((prevStars) =>
+      event.target.checked
+        ? [...prevStars, starRating]
+        : prevStars.filter((star) => star !== starRating)
+    );
+  };
+
+  const handleHotelTypesChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const hotelType = event.target.value;
+
+    setSelectedHotelTypes((prevHotelTypes) =>
+      event.target.checked
+        ? [...prevHotelTypes, hotelType]
+        : prevHotelTypes.filter((hotel) => hotel !== hotelType)
+    );
+  };
+
+  const handleFacliltiesChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const facility = event.target.value;
+
+    setSelectedFacilities((prevFacility) =>
+      event.target.checked
+        ? [...prevFacility, facility]
+        : prevFacility.filter((hotel) => hotel !== facility)
+    );
+  };
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter by:
+          </h3>
+          <StarRatingFilter
+            selectedStars={selectedStars}
+            onChange={handleStarsChange}
+          />
+          <HotelTypesFilter
+            selectedHotelTypes={selectedHotelTypes}
+            onChange={handleHotelTypesChange}
+          />
+          <FacilitiesFilter
+            selectedFacilities={selectedFacilities}
+            onChange={handleFacliltiesChange}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <span className="text-xl font-bold">
+            {hotelData?.pagination.total} Hotels found
+            {search.destination ? ` in ${search.destination}` : ""}
+          </span>
+          {/* todo sort option */}
+        </div>
+        {hotelData?.data.map((hotel) => (
+          <SearchResultsCard hotel={hotel} />
+        ))}
+      </div>
+      <Pagination
+        page={hotelData?.pagination.page || 1}
+        pages={hotelData?.pagination.pages || 1}
+        onPageChange={(page) => setPage(page)}
+      />
+    </div>
+  );
 };
 
 export default Search;
