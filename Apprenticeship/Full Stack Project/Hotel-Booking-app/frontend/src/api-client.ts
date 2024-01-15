5b3eeb692ad0d6e40cc9ca010ce69f8089c3d928
ShirajShrestha <shresthashiraj333@gmail.com>
2024-01-15 11:56:22 +0545

    Add Stripe payment and save Booking data in database

--- Apprenticeship/Full Stack Project/Hotel-Booking-app/frontend/src/api-client.ts
+++ Apprenticeship/Full Stack Project/Hotel-Booking-app/frontend/src/api-client.ts
@@ -3,8 +3,10 @@ import { SingInFormData } from "./pages/SignIn";
 import {
   HotelSearchResponse,
   HotelType,
+  PaymentIntentResponse,
   UserType,
 } from "../../backend/src/shared/types";
+import { BookingFormData } from "./forms/BookingForm/BookingForm";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
@@ -174,4 +176,45 @@ export const fetchHotelById = async (hotelId: string): Promise<HotelType> => {
     throw new Error("Error fetching Hotels");
   }
   return response.json();
+};
+
+export const createPaymentIntent = async (
+  hotelId: string,
+  numberOfNights: string
+): Promise<PaymentIntentResponse> => {
+  const response = await fetch(
+    `${API_BASE_URL}/api/hotels/${hotelId}/bookings/payment-intent`,
+    {
+      credentials: "include",
+      method: "POST",
+      body: JSON.stringify({ numberOfNights }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Error fetching payment intent");
+  }
+
+  return response.json();
+};
+
+export const createRoomBooking = async (formData: BookingFormData) => {
+  const response = await fetch(
+    `${API_BASE_URL}/api/hotels/${formData.hotelId}/bookings`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formData),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error("Error booking room");
+  }
 };
