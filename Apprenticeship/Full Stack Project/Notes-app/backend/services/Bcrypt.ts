f61efa3f3ed2ef3a77385db9730b2394ac44034f
ShirajShrestha <shresthashiraj333@gmail.com>
2024-01-24 23:26:15 +0545

    Install bcrypt

--- Apprenticeship/Full Stack Project/Notes-app/backend/services/Bcrypt.ts
+++ Apprenticeship/Full Stack Project/Notes-app/backend/services/Bcrypt.ts
@@ -1,1 +1,13 @@
-import bcrypt from "bcrypt";
+const { Sequelize, DataTypes } = require("sequelize");
+const useBcrypt = require("sequelize-bcrypt");
+
+const database = new Sequelize({
+  ...sequelizeConnectionOptions,
+});
+
+const User = database.define("User", {
+  email: { type: DataTypes.STRING },
+  password: { type: DataTypes.STRING },
+});
+
+useBcrypt(User, options);
