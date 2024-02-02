c4f540b1d28db6e52de6e52b2a0024c9d57085ae
ShirajShrestha <shresthashiraj333@gmail.com>
2024-02-02 22:25:23 +0545

    Start Reset password feat

---
+++ Apprenticeship/Full Stack Project/Notes-app/backend/services/ResetPassword.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export const forgotPassword = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const resetPassword = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
